39069baa498d05ec4041b74239225dca59e8fd3c
Ziqiang Li <ziqiang.lee@gmail.com>
2021-02-26 21:04:23 +0800

    fix: fix path

--- download.sh
+++ download.sh
@@ -14,6 +14,6 @@ echo "视频下载完成！"
 
 echo "转码中..."
 while read c1 c2 c3; do
-    ffmpeg -i "$c3.ts" -c:v libx264 -c:a aac "$c3.mp4"
+    ffmpeg -i video/"$c3.ts" -c:v libx264 -c:a aac video/"$c3.mp4"
 done < list.txt
 echo "转码完成！"
