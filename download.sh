39d884d098a02c407353d1f6f32544c3d643e296
Ziqiang Li <ziqiang.lee@gmail.com>
2021-02-27 11:49:06 +0800

    feat: 优化

--- download.sh
+++ download.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-echo "视频下载中..."
-mkdir -p video
-./m3u8_downloader -f=list_video.txt -o=./video
-echo "视频下载完成！"
-
-echo "音频下载中..."
-mkdir -p audio
-rm -rf audio/*
-while read c1 c2; do
-    wget "$c1" -O ./audio/"$c2.mp3"
-done < list_audio.txt
-echo "音频下载完成！"
+. download_video.sh
+. download_audio.sh
--- download_video.sh
+++ download_video.sh
@@ -3,5 +3,9 @@
 echo "视频下载中..."
 mkdir -p video
 rm -rf video/*
-./m3u8_downloader -f=list_video.txt -o=./video
+# ./m3u8_downloader -f=list_video.txt -o=./video
+while read c1 c2; do
+    ./m3u8_downloader -u=$c1 -o=./video
+    mv video/main.ts video/"$c2.ts"
+done < list_video.txt
 echo "视频下载完成！"
